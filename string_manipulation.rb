c13b2e346303706dce473a3f15f1e28e63b6c4b3
Severin Wiggenhorn <severin.randall@gmail.com>
2017-09-25 18:02:59 -0700

    Initial Commit

--- string_manipulation.rb
+++ string_manipulation.rb
@@ -1,30 +1,127 @@
 # A method to reverse a string in place.
 def string_reverse(my_string)
-  puts "NOT IMPLEMENTED"
+  length = my_string.length
+  i = 0
+
+  if length >= 2
+  (length / 2).times do
+    temp = my_string[i]
+    my_string[i] = my_string[length - 1]
+    my_string[length - 1] = temp
+    i += 1
+    length -= 1
+    end
+  end
+  return my_string
 end
 
 # A method to reverse each word in a sentence, in place.
 def reverse_words(my_words)
-  puts "NOT IMPLEMENTED"
+  length = my_words.length
+
+  word_start = 0
+  word_end = 0
+
+  length.times do |i|
+    if my_words[i] == " " || i == length - 1
+
+      if my_words[i] == " "
+        word_end = i - 1
+      else
+        word_end = i
+      end
+
+      my_words[word_start..word_end] = string_reverse(my_words[word_start..word_end])
+      word_start = i + 1
+      word_end = i + 1
+    end
+    i += 1
+  end
+  return my_words
 end
 
 # A method to reverse the words in a sentence, in place.
 def reverse_sentence(my_sentence)
-  puts "NOT IMPLEMENTED"
+  my_sentence_reversed = string_reverse(my_sentence)
+  corrected_sentence = reverse_words(my_sentence_reversed)
+  return corrected_sentence
 end
 
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
 def palindrome_check(my_phrase)
-  puts "NOT IMPLEMENTED"
+  if my_phrase.length > 0
+    beg = 0
+    fin = my_phrase.length - 1
+
+    ((my_phrase.length) / 2).times do
+
+      while my_phrase[beg] == " "
+        beg += 1
+      end
+
+      while my_phrase[fin] == " "
+        fin -= 1
+      end
+
+      if my_phrase[beg] != my_phrase[fin]
+        return false
+      end
+    end
+  end
   return true
 end
+
 
 # A method that updates the string by replacing consecutive repeating characters
 # with a number representing the frequncy. The replacement is done only if the
 # string length will get reduced by the process.
 def encode_repeating(my_string)
-  puts "NOT IMPLEMENTED"
+  counter = 1
+  start = 0
+  finish = 0
+  i = 0
+
+  until i > my_string.length do
+
+    if i + 1 == my_string.length && counter >= 2
+      my_string[start] = my_string[start] + counter.to_s
+      i += 1
+      break
+    elsif (my_string[i] != my_string[i + 1]) && counter <= 2
+      start = i + 1
+      finish = i + 1
+      counter = 1
+      i += 1
+      next
+    elsif
+      (my_string[i] != my_string[i + 1]) && counter >= 3
+      my_string[start] = my_string[start] + counter.to_s
+      start = i += 1
+      counter = 1
+    else
+      counter += 1
+      finish += 1
+      i += 1
+    end
+  end
+
+  if /[0-9]/.match(my_string)
+    length = my_string.length
+    i = 0
+    until i > length do
+      if my_string[i].to_i > 0
+        num = my_string[i].to_i
+        my_string.slice!((i + 1), (num - 1))
+        i += 1
+        length = my_string.length
+      else
+        i += 1
+      end
+    end
+  end
+
+  return my_string
 end
 
 ### ---- END OF METHODS
@@ -67,27 +164,27 @@ puts "BUG: madam is a palindrome and should return true" if palindrome_check(phr
 phrase = "empty"
 puts "BUG: empty is not a palindrome and should return false" if palindrome_check(phrase) != false
 # optional challenge
-# phrase = "nurses run"
-# puts "BUG: 'nurses run' is a palindrome and should return true" if palindrome_check(phrase) != true
+phrase = "nurses run"
+puts "BUG: 'nurses run' is a palindrome and should return true" if palindrome_check(phrase) != true
 puts "Palindrome test complete."
 
 # Optional Question #5
-# puts "Test 5: Encode test"
-# test1 = "aaabbbbbcccc"
-# encode_repeating(test1)
-# if test1 != "a3b5c4"
-#   puts "BUG! 'aaabbbbbcccc' should get encoded to 'a3b5c4', not '#{test1}'"
-# end
+puts "Test 5: Encode test"
+test1 = "aaabbbbbcccc"
+encode_repeating(test1)
+if test1 != "a3b5c4"
+  puts "BUG! 'aaabbbbbcccc' should get encoded to 'a3b5c4', not '#{test1}'"
+end
 #
-# test2 = "xxxyttttgeee"
-# encode_repeating(test2)
-# if test2 != "x3yt4ge3"
-#   puts "BUG! 'xxxyttttgeee' should get encoded to 'x3yt4ge3', not '#{test2}'"
-# end
-#
-# test3 = "ddbbfffgjjjj"
-# encode_repeating(test3)
-# if test3 != "ddbbf3gj4"
-#   puts "BUG! 'ddbbfffgjjjj' should get encoded to 'ddbbf3gj4', not '#{test3}'"
-# end
-# puts "Encode test complete."
+test2 = "xxxyttttgeee"
+encode_repeating(test2)
+if test2 != "x3yt4ge3"
+  puts "BUG! 'xxxyttttgeee' should get encoded to 'x3yt4ge3', not '#{test2}'"
+end
+
+test3 = "ddbbfffgjjjj"
+encode_repeating(test3)
+if test3 != "ddbbf3gj4"
+  puts "BUG! 'ddbbfffgjjjj' should get encoded to 'ddbbf3gj4', not '#{test3}'"
+end
+puts "Encode test complete."
